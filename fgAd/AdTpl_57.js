5d2d57953413579781cccbd22c69795b27e03e69
nobita <unvisityboss@gmail.com>
2015-04-08 17:21:41 +0800

    修改了 floor 的一些樣式

--- fgAd/AdTpl_57.js
+++ fgAd/AdTpl_57.js
@@ -6,10 +6,12 @@ function Tpl_57(Loc) {
             img_h = AdMap[Loc][N]["C"]["i"][2],
             link  = AdMap[Loc][N]["C"]["l"][theChan],
             H = '<div id="ad-floor" class="ad-content">' +
+                '<div class="ad-content-box">' +
                 '<a class="ad-close-btn" href="javascript:;">×</a>' +
                 '<a class="ad-link" href="' + link + '" target="_blank">' +
-                '<img class="ad-image" src="' + img + '" alt="' + img + '" style="height:' + h + 'px;width:' + w + 'px">' +
+                '<img class="ad-image" src="' + img + '" alt="' + img + '" style="height:' + img_h + 'px;width:' + img_w + 'px">' +
                 '</a>' +
+                '</div>' +
                 '</div>';
 
         return H;
